1a3669a4bf9a77c9adb276607d1a3897e10a450b
sukhi <your.email@example.com>
2024-07-07 20:27:25 -0400

    added

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
@@ -5,25 +5,19 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import ca.georgiancollege.ice7.databinding.TextRowItemBinding
 
-class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit):
-    ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator())
-{
+class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit) :
+    ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator()) {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder
-        {
-            val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return TVShowViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder {
+        val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TVShowViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TVShowViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
         }
-
-        override fun onBindViewHolder(holder: TVShowViewHolder, position: Int)
-        {
-            val current = getItem(position)
-
-            holder.bind(current)
-
-            holder.itemView.setOnClickListener {
-                onItemClicked(current)
-            }
-        }
-
+    }
 }
